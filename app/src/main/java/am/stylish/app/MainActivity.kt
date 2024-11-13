3659c5489728f0fdd7cff4c14b0b7b97d785690c
vardanyan-karen <armen1979vardanyan@gmail.com>
2024-11-13 23:13:26 +0400

    Added SplashScreen

--- app/src/main/java/am/stylish/app/MainActivity.kt
+++ app/src/main/java/am/stylish/app/MainActivity.kt
@@ -2,14 +2,21 @@ package am.stylish.app
 
 import am.stylish.app.navigation.AppNavigation
 import am.stylish.app.navigation.destination.AppDestination
+import am.stylish.app.splash.presentation.SplashScreen
 import android.app.Activity
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.view.WindowInsetsControllerCompat
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +36,18 @@ class MainActivity : ComponentActivity() {
                 activity.window.isNavigationBarContrastEnforced = false
             }
 
-            AppNavigation(startDestination = AppDestination.Auth)
+            var showSplashScreen by remember { mutableStateOf(true) }
+
+            LaunchedEffect(Unit) {
+                delay(2500)
+                showSplashScreen = false
+            }
+
+            if (showSplashScreen) {
+                SplashScreen()
+            } else {
+                AppNavigation(startDestination = AppDestination.Auth)
+            }
         }
     }
 }
