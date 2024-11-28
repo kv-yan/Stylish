6b462347cbba85fd825c1520c55b232bff7a63dc
vardanyan-karen <armen1979vardanyan@gmail.com>
2024-11-29 02:19:27 +0400

    Added search bar to app

--- app/src/main/java/am/stylish/app/main/home/HomeScreen.kt
+++ app/src/main/java/am/stylish/app/main/home/HomeScreen.kt
@@ -3,6 +3,7 @@ package am.stylish.app.main.home
 import am.stylish.app.R
 import am.stylish.app.common_domain.model.product.Product
 import am.stylish.app.common_domain.model.special_offer.SpecialOffer
+import am.stylish.app.common_presentation.components.search.SearchBar
 import am.stylish.app.common_presentation.components.action_bar.AppActionBar
 import am.stylish.app.common_presentation.components.button.IconButton
 import am.stylish.app.common_presentation.components.product_list.ProductListWithSpecialOffers
@@ -10,6 +11,7 @@ import am.stylish.app.common_presentation.components.text.AppLogoWithText
 import am.stylish.app.common_presentation.ui.theme.ActionBarLogoTextStyle
 import am.stylish.app.common_presentation.utils.test_mock_data.mockPageProductData
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -22,7 +24,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -32,9 +36,11 @@ fun HomeScreen(
     onSpecialOfferClick: (SpecialOffer) -> Unit = {}
 ) {
     val scrollState = rememberScrollState()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .background(Color(0xFFFDFDFD))
             .verticalScroll(scrollState)
     ) {
         AppActionBar(modifier = Modifier
@@ -64,6 +70,13 @@ fun HomeScreen(
                     contentDescription = null
                 )
             })
+
+        SearchBar(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp, top = 16.dp),
+            text = stringResource(R.string.search_any_product),
+        )
 
         ProductListWithSpecialOffers(
             modifier = modifier.fillMaxSize(),
